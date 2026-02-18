45d3a1d33eb175acf0a7cae2f05ca8adce00f865
Sujay K C <sujaykc596@gmail.com>
2026-02-18 05:50:36 +0530

    Refactor user signup route and improve error handling

--- src/app.js
+++ src/app.js
@@ -5,33 +5,27 @@ const port = 3000;
 const connectDB = require('../config/database');
 const User = require('./models/userModel');
 
-app.post('/signup', async (req, res)=>{
-  const userData = new User({
-    fullName : "Sujay",
-    email : "sujay@gmail.com",
-    password : "Sujay@03",
-    age : 24,
-    gender : "male"
-  });
 
-  try{
-    userData.save();
-    res.send("User data saved in the db");
-  }catch(err){
-    res.status(400).send("Storing user data is failed");
-  }
+app.post('/signUP', (req, res)=>{
+  const userData = new User({
+    firstName : "Sujay",
+    lastName : "gowda",
+    age : 24,
+    role : "backend developer",
+    package : "12 lpa"
+  })
+  userData.save();
+  res.send(userData);
+})
+connectDB().then(()=>{
+  console.log("MongoDB connected successfully");
+  app.listen(port, ()=>{
+    console.log("Server connected successfully");
+  })
+}).catch((err)=>{
+  console.log("DB connection failed "+ err.message);
 })
 
-
- connectDB().then(()=>{
-  console.log("MOngoDB connected succesfully");
-  app.listen(port, ()=>{
-    console.log("server connected successfully");
-  })
- }).catch((err)=>{
-  console.log("DB connection failed "+ err);
- })
- 
 
 
 
